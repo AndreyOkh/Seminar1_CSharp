ac4e4ec25c45ed82335efe28ccfc0f3b47a897eb
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-06 23:29:50 +1000

    Project_005 Написал программу которая на вход получает трехзначное число и на выходе показывает последнюю цифру этого числа

--- Project_005/Program.cs
+++ Project_005/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+*/
+Console.Write("Введите трехзначное число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int result = number % 10;
+
+Console.WriteLine(result);
