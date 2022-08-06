44371da9e8a947db23ad8477368dcb7f6af19f1e
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-06 22:54:20 +1000

    Написал программу которая получает на вход 2 числа и проверяет является ли второе квадратом первого

--- Project_002/Program.cs
+++ Project_002/Program.cs
@@ -1,3 +1,18 @@
 ﻿/*
 Задача написать программу которая на вход принимает 2 числа и проверяет явзляется ли второе квадратом первого
 */
+Console.Write("Введите первое число: ");
+string inputFirstNum = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string inputSecondNum = Console.ReadLine();
+int firstNum = Convert.ToInt32(inputFirstNum);
+int secondNum = Convert.ToInt32(inputSecondNum);
+
+int sqrFirstNum = firstNum * firstNum;
+
+if (sqrFirstNum == secondNum) {
+    Console.WriteLine("Число " + secondNum + " является квадратом числа " + firstNum);
+}
+else {
+    Console.WriteLine("Число " + secondNum + " не является квадратом числа " + firstNum);
+}
