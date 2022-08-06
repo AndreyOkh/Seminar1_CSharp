b0591a6c22ef0ea13c77a03b986ba2687c7ab871
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-06 23:24:41 +1000

    Project_004 Написал программу которая получает на вход число и выводит все числа в промежутке от -N до N

--- Project_004/Program.cs
+++ Project_004/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int num = Convert.ToInt32(input);
+int minNum = -num;
+
+while (minNum <= num) {
+    Console.WriteLine(minNum);
+    minNum++;
+}
