96ddc7fd0cb25e32f66ff011c75cba6d430e64d0
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-06 23:04:55 +1000

    Написал программу которая выдает день недели по заданному номеру

--- Project_003/Program.cs
+++ Project_003/Program.cs
@@ -1,3 +1,38 @@
 ﻿/*
 Написать программу которая будет выдавать день недели по заданному номеру
 */
+Console.Write("Введите номер дня недели: ");
+string input = Console.ReadLine();
+
+string monday = "Понедельник";
+string tuesday = "Вторник";
+string wednesday = "Среда";
+string thursday = "Четверг";
+string friday = "Пятница";
+string saturday = "Суббота";
+string sunday = "Воскресенье";
+
+if (input == "1") {
+    Console.WriteLine(monday);
+}
+else if (input == "2") {
+    Console.WriteLine(tuesday);
+}
+else if (input == "3") {
+    Console.WriteLine(wednesday);
+}
+else if (input == "4") {
+    Console.WriteLine(thursday);
+}
+else if (input == "5") {
+    Console.WriteLine(friday);
+}
+else if (input == "6") {
+    Console.WriteLine(saturday);
+}
+else if (input == "7") {
+    Console.WriteLine(sunday);
+}
+else {
+    Console.WriteLine("Значение введено не верно! Попробуйте еще раз.");
+}
