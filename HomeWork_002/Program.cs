17b1aef4e6e171e46e04d24e1663b0c5eb8638cd
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-07 00:03:20 +1000

    HomeWork_002 Написал программу, которая принимает на вход 2 числа и выдает какое число больше

--- HomeWork_002/Program.cs
+++ HomeWork_002/Program.cs
@@ -4,3 +4,20 @@ a = 5; b = 7 ->  max = 7
 a = 2; b = 10 -> max = 10
 a = -9; b = -3 -> max = -3
 */
+Console.Write("Введи первое число: ");
+string firstNumberInput = Console.ReadLine();
+Console.Write("Введи второе число: ");
+string secondNumberInput = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstNumberInput);
+int secondNumber = Convert.ToInt32(secondNumberInput);
+
+if (firstNumber > secondNumber) {
+    Console.WriteLine(firstNumber);
+}
+else if (firstNumber < secondNumber) {
+    Console.WriteLine(secondNumber);
+}
+else {
+    Console.WriteLine("Значеения равны");
+}
