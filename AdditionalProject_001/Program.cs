c3d962faeed569de53865a8efc9f1407a1714aab
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-07 20:40:05 +1000

    AdditionalProject_001 Написал программу которая при успешном вводе пароля выдает сообщение.

--- AdditionalProject_001/Program.cs
+++ AdditionalProject_001/Program.cs
@@ -3,3 +3,27 @@
 Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
 */
 
+string message = "Top secret message";
+string passwd = "passwd";
+string userPasswd;
+
+int countPasswd = 0;
+
+while (countPasswd < 3) {
+    Console.Write("Введите пароль: ");
+    userPasswd = Console.ReadLine();
+    if (userPasswd == passwd) {
+        Console.WriteLine("Сообщение: " + message);
+        countPasswd = 999;
+    }
+    else {
+        Console.WriteLine("Не верный пароль, попробуйте еще раз!");
+        countPasswd++;
+    }
+}
+if (countPasswd == 999) {
+    Console.WriteLine("The end!");
+}
+else {
+    Console.WriteLine("Попытки ввода пароля закончились!");
+}
