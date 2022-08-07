774088b4529bc521a132a3159b784f220b0bbfde
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-07 20:58:41 +1000

    AdditionalProject_002 Написал программу с бесконечным циклом которая ожидает команды пользователя что бы прекратить.

--- AdditionalProject_002/Program.cs
+++ AdditionalProject_002/Program.cs
@@ -1,3 +1,13 @@
 ﻿/*
 2. Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
 */
+
+string command = "";
+int i = 0;
+
+ while (command != "exit") {
+    Console.WriteLine(i + " ");
+    i++;
+    Console.Write("Введите команду exit что бы прекратить: ");
+    command = Console.ReadLine();
+}
