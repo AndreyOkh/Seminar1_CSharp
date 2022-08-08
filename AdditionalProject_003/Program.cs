5d1f652bbab99058fce5f0de09ffab8cd4998867
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-08 23:45:48 +1000

    AdditionalProject_003 Написал игру угадайка. Программа загадывает число, а пользователь угаадывает, что бы угадать дается 3 попытки, если ответ не верен программа подсказывает больше или меньше число пользователя относсительно загаданного

--- AdditionalProject_003/Program.cs
+++ AdditionalProject_003/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ,
+// то программа сообщает, больше загаданное число или меньше. На отгадывание дается 3 попытки.
+
+int secretNumber = new Random().Next(1, 10);
+int countUserTry = 0;
+int maxUserTry = 3;
+
+while (countUserTry < maxUserTry) {
+    Console.Write("Введите число: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber == secretNumber) {
+        Console.WriteLine("You WIN!");
+        break;
+    }
+    else if (userNumber < secretNumber) {
+        Console.WriteLine("Ваше число меньше загаданного!");
+        countUserTry++;
+    }
+    else if (userNumber > secretNumber) {
+        Console.WriteLine("Ваше число больше загаданного!");
+        countUserTry++;
+    }
+}
+if (countUserTry == 3) {
+    Console.WriteLine("Вы проиграли, было загадано число " + secretNumber);
+}
+
