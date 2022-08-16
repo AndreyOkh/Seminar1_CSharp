130becde47aa1eee905befb00d3a21e9ef42d206
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-16 10:01:49 +1000

    7. Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке.

---
+++ AdditionalProject_007/Program.cs
@@ -0,0 +1,22 @@
+﻿// 7. Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+
+int[] array = {1 ,2 ,4 ,6 ,7 ,8 ,5 ,3 ,2};
+
+Shuffle(array);
+
+foreach (int item in array)
+{
+    Console.Write($"{item} ");
+}
+
+void Shuffle (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int randomeIndex = new Random().Next(i, array.Length);
+
+        int temp = array[i];
+        array[i] = array[randomeIndex];
+        array[randomeIndex] = temp;
+    }
+}
